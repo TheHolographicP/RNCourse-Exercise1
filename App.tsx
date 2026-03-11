9e991115a0f378c0a6658d47bccc18b80b6d684b
TheHolographicP <thegamblerkid@gmail.com>
2026-03-11 10:19:47 -0600

    Basic app layout, start wiring up input;

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+
 
 export default function App() {
+  const [goalInputText, setGoalInputText] = useState('')
+
+
+  function goalInputHandler(enteredText: string)
+  {
+    setGoalInputText(enteredText)
+  }
+  function addGoalHandler()
+  {
+    console.log(goalInputText)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app again! Hello!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.goalInput} onChangeText={goalInputHandler}/>
+        <Button title="Add Goal" onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>Big list of goals</Text>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop:50,
+    paddingHorizontal:16,
+    flex:1
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    gap:10,
+    alignItems: 'center',
+    marginBottom: 16
+  },
+  goalInput:{
+    flex: 1,
+    borderWidth: 1,
+    padding:3
+  },
+  goalsContainer:{
+    flex: 1,
+    borderTopColor: 'grey',
+    borderTopWidth: 1
+  }
 });
